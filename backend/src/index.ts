17c4b2f574ef4554b87ad16d38790c2ba7a88428
Jack Pfeiffer <jl5her@yahoo.com>
2024-02-02 00:19:56 -0500

    Change ports

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ import { Maze } from "./models/maze";
 import { Player } from "./models/player";
 
 const host = process.env.HOST || '0.0.0.0'
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 const app = express()
 
